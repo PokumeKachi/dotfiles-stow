6ad21da85e14419ffe12ca3c300ecbbaa5067f2f
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-06 21:44:16 +0700

    feat: added cwd to winbar

--- config/nvim/lazy/oil.lua
+++ config/nvim/lazy/oil.lua
@@ -1,3 +1,14 @@
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
 	"stevearc/oil.nvim",
 	-- lazy = false,
@@ -22,6 +33,7 @@ return {
 			list = false,
 			conceallevel = 3,
 			concealcursor = "nvic",
+			winbar = "%!v:lua.get_oil_winbar()",
 		},
 		view_options = { show_hidden = true, show_parent_dir = false },
 	},
