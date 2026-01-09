42f7ccd80a705e9363008a34289f2122a8ca74f5
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-09 16:06:09 +0700

    feat: fixed oil.lua option being out of the array

--- config/nvim/lazy/oil.lua
+++ config/nvim/lazy/oil.lua
@@ -1,12 +1,12 @@
 function _G.get_oil_winbar()
-  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-  local dir = require("oil").get_current_dir(bufnr)
-  if dir then
-    return vim.fn.fnamemodify(dir, ":~")
-  else
-    -- If there is no current directory (e.g. over ssh), just show the buffer name
-    return vim.api.nvim_buf_get_name(0)
-  end
+	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local dir = require("oil").get_current_dir(bufnr)
+	if dir then
+		return vim.fn.fnamemodify(dir, ":~")
+	else
+		-- If there is no current directory (e.g. over ssh), just show the buffer name
+		return vim.api.nvim_buf_get_name(0)
+	end
 end
 
 return {
@@ -36,8 +36,8 @@ return {
 			winbar = "%!v:lua.get_oil_winbar()",
 		},
 		view_options = { show_hidden = true, show_parent_dir = false },
+		delete_to_trash = true,
 	},
-	delete_to_trash = true,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = { { "-", "<cmd>Oil<cr>", desc = "oil" } },
 }
