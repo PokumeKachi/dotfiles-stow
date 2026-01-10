d606d18062d0e3fe8159e3e81666f41315407684
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-10 11:08:38 +0700

    feat: addeds stuff
    - kitty: made the cursor stand out more
    - neovim: added keymap for focusing documentation popup when in insert mode
    - neovim: added hardtime.nvim
    - neovim: added gruvbox to colorschemes
    - neovim: added luasnip, working on it later

--- config/nvim/lazy/colorscheme.lua
+++ config/nvim/lazy/colorscheme.lua
@@ -1,15 +1,22 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	lazy = false,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-		})
-		vim.cmd.colorscheme("catppuccin")
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
 
-		vim.api.nvim_set_hl(0, "LineNr", { fg = "#fff5f5", bg = "#1e1e1e" }) -- normal line numbers
-		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffafa0", bold = true }) -- current line number
-	end,
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#fff5f5", bg = "#1e1e1e" }) -- normal line numbers
+			vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffafa0", bold = true }) -- current line number
+		end,
+	},
 }
