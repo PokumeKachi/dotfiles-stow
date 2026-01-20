d02a21a53e86c2641013273ae3e910a921f1db5a
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-20 21:23:26 +0700

    feat: added an echo to the entry of flake shell

--- config/nvim/snippets/just.lua
+++ config/nvim/snippets/just.lua
@@ -32,6 +32,7 @@ return {
 			tab(2) .. "h=$(nix hash path ./flake.nix); \\",
 			tab(2) .. 'old="$FLAKE_HASH"; \\',
 			tab(2) .. 'if [ "$old" != "$h" ]; then \\',
+			tab(3) .. "echo \"Entering flake shell...\"; \\",
 			tab(3) .. 'FLAKE_HASH="$h" exec nix develop; \\',
 			tab(2) .. "else \\",
 			tab(3) .. "echo \"Inside flake shell, skipping nix develop...\"; \\",
