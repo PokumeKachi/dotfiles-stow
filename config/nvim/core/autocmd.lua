1c0dad82dd4555ea3484f196f65ecfc0a7e3bcac
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-30 23:21:11 +0700

    feat: added autocmd that prevents duplicated buffers in multi windows

--- config/nvim/core/autocmd.lua
+++ config/nvim/core/autocmd.lua
@@ -1,5 +1,40 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    if vim.g.__buf_dedupe_in_progress then return end
+    vim.g.__buf_dedupe_in_progress = true
+
+    local opened_buf = vim.api.nvim_get_current_buf()
+    local opened_win = vim.api.nvim_get_current_win()
+
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if win ~= opened_win and vim.api.nvim_win_get_buf(win) == opened_buf then
+        -- get the alternate buffer *for the opened window*
+        local alt_buf
+        vim.api.nvim_win_call(opened_win, function()
+          alt_buf = vim.fn.bufnr("#")
+        end)
+
+        -- focus the already-visible window
+        vim.api.nvim_set_current_win(win)
+
+        -- if the opened window has a usable alternate, show it; otherwise close it
+        if alt_buf and alt_buf > 0 and vim.api.nvim_buf_is_valid(alt_buf) and vim.api.nvim_buf_is_loaded(alt_buf) then
+          vim.api.nvim_win_set_buf(opened_win, alt_buf)
+        else
+          vim.api.nvim_win_close(opened_win, true)
+        end
+
+        vim.g.__buf_dedupe_in_progress = false
+        return
+      end
+    end
+
+    vim.g.__buf_dedupe_in_progress = false
+  end,
+})
+
 autocmd("TermOpen", {
 	callback = function()
 		vim.opt_local.relativenumber = true
