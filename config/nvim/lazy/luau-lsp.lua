ff3ce1cf7f0405de00382df728eea27fc4cb3f56
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-02-07 15:30:27 +0700

    feat: added selene support

--- config/nvim/lazy/luau-lsp.lua
+++ config/nvim/lazy/luau-lsp.lua
@@ -1,12 +1,18 @@
 return {
 	"lopi-py/luau-lsp.nvim",
 	opts = {
-		-- platform = {
-		-- 	type = "roblox",
-		-- },
-		-- types = {
-		-- 	roblox_security_level = "PluginSecurity",
-		-- },
+		platform = {
+			type = "roblox",
+		},
+		types = {
+			roblox_security_level = "PluginSecurity",
+		},
+		sourcemap = {
+			enabled = true,
+			autogenerate = true,
+			rojo_project_file = "default.project.json",
+			sourcemap_file = "sourcemap.json",
+		},
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
