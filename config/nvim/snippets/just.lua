4a7e6028eacb1fc6005bf96b89230bf34af77204
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-03-08 20:44:24 +0700

    feat: we shouldn't use just develop, use direnv

--- config/nvim/snippets/just.lua
+++ config/nvim/snippets/just.lua
@@ -20,20 +20,20 @@ return {
 	s("template", {
 		t(base),
 	}),
-	s("template_with_flake", {
-		t(vim.list_extend(vim.deepcopy(base), {
-			"",
-			"develop:",
-			tab() .. "@sh -c ' \\",
-			tab(2) .. "h=$(nix hash path ./flake.nix); \\",
-			tab(2) .. 'old="$FLAKE_HASH"; \\',
-			tab(2) .. 'if [ "$old" != "$h" ]; then \\',
-			tab(3) .. "echo \"Entering flake shell...\"; \\",
-			tab(3) .. 'FLAKE_HASH="$h" exec nix develop; \\',
-			tab(2) .. "else \\",
-			tab(3) .. "echo \"Inside flake shell, skipping nix develop...\"; \\",
-			tab(2) .. "fi \\",
-			tab() .. "'",
-		})),
-	}),
+	-- s("template_with_flake", {
+	-- 	t(vim.list_extend(vim.deepcopy(base), {
+	-- 		"",
+	-- 		"develop:",
+	-- 		tab() .. "@sh -c ' \\",
+	-- 		tab(2) .. "h=$(nix hash path ./flake.nix); \\",
+	-- 		tab(2) .. 'old="$FLAKE_HASH"; \\',
+	-- 		tab(2) .. 'if [ "$old" != "$h" ]; then \\',
+	-- 		tab(3) .. "echo \"Entering flake shell...\"; \\",
+	-- 		tab(3) .. 'FLAKE_HASH="$h" exec nix develop; \\',
+	-- 		tab(2) .. "else \\",
+	-- 		tab(3) .. "echo \"Inside flake shell, skipping nix develop...\"; \\",
+	-- 		tab(2) .. "fi \\",
+	-- 		tab() .. "'",
+	-- 	})),
+	-- }),
 }
