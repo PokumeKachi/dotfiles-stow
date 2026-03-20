4bdba35ea430ae17992e54c0a3a74740b4bb5323
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-03-20 18:31:02 +0700

    feat: back

--- config/nvim/lazy/vimtex.lua
+++ config/nvim/lazy/vimtex.lua
@@ -7,32 +7,32 @@ return {
 		vim.g.vimtex_view_method = "general"
 		vim.g.vimtex_view_general_viewer = "zathura"
 
-		-- vim.g.vimtex_compiler_method = "latexmk"
-		-- vim.g.vimtex_compiler_latexmk = {
-		-- 	build_dir = "build",
-		-- 	continuous = 1,
-		-- 	executable = "latexmk",
-		-- 	options = {
-		-- 		"-pdf",
-		-- 		"-interaction=nonstopmode",
-		-- 		"-synctex=1",
-		-- 		"-file-line-error",
-		-- 		"-halt-on-error",
-		-- 	},
-		-- }
-
-		vim.g.vimtex_compiler_method = "tectonic"
-
-		vim.g.vimtex_compiler_tectonic = {
+		vim.g.vimtex_compiler_method = "latexmk"
+		vim.g.vimtex_compiler_latexmk = {
 			build_dir = "build",
 			continuous = 1,
-			executable = "tectonic",
+			executable = "latexmk",
 			options = {
-				"--synctex",
-				"--keep-logs",
-				"--keep-intermediates",
+				"-pdf",
+				"-interaction=nonstopmode",
+				"-synctex=1",
+				"-file-line-error",
+				"-halt-on-error",
 			},
 		}
+
+		-- vim.g.vimtex_compiler_method = "tectonic"
+		--
+		-- vim.g.vimtex_compiler_tectonic = {
+		-- 	build_dir = "build",
+		-- 	continuous = 0, -- MUST be 0
+		-- 	executable = "tectonic",
+		-- 	options = {
+		-- 		"--synctex",
+		-- 		"--keep-logs",
+		-- 		"--keep-intermediates",
+		-- 	},
+		-- }
 
 		vim.g.vimtex_compiler_silent = 1
 		vim.g.vimtex_quickfix_mode = 0
