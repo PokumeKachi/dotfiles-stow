bd80393ece3e4d88380863a5929c2bbbb31bec9d
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-06-23 14:03:00 +0700

    feat: fix stuff

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -6,7 +6,6 @@
 
     home.stateVersion = "26.05";
 
-    # Packages Home Manager will install
     home.packages = with pkgs; [
         direnv
         just
@@ -24,37 +23,24 @@
     ];
 
     home.pointerCursor = {
-        package = pkgs.gnome-themes-extra; # e.g., pkgs.catppuccin-cursors
-        name = "Adwaitda-dark";
+        package = pkgs.gnome-themes-extra;
+        name = "Adwaita-dark";
         size = 24;
-        gtk.enable = true; # This is important for GTK apps
-        x11.enable = true; # Good for XWayland compatibility
+        gtk.enable = true;
+        x11.enable = true;
     };
-    # direnv with nix-direnv integration
     programs.direnv = {
         enable = true;
         nix-direnv.enable = true;
     };
 
-    # Optional: shell configuration (HM can manage your shell)
-    # programs.zsh = {
-    #     enable = true;
-    #     shellAliases = {
-    #         ll = "ls -l";
-    #         la = "ls -a";
-    #         update = "home-manager switch";
-    #     };
-    # };
 
-    # Environment variables
     home.sessionVariables = {
         EDITOR = "nvim";
         VISUAL = "nvim";
     };
 
-    # IMPORTANT: Let Stow manage your dotfiles, not HM
     home.file = { };
 
-    # Let home-manager manage itself
     programs.home-manager.enable = true;
 }
