122350ff4391ce31120ecb098e7a04ed6a15473c
PokumeKachi <108186213+PokumeKachi@users.noreply.github.com>
2026-09-09 07:06:59 +0700

    feat: <leader>qbc no longer quits the window

--- config/nvim/lua/core/keymaps/buffer.lua
+++ config/nvim/lua/core/keymaps/buffer.lua
@@ -2,11 +2,11 @@ local map = require("utils.keymap").map
 
 local buffer = require("utils.buffer")
 
-map("n", "<leader>qbc", function()
-	vim.api.nvim_buf_delete(vim.api.nvim_get_current_buf(), { force = false })
-end, {
-	desc = "Quit Current Buffer",
-})
+vim.keymap.set("n", "<leader>qbc", function()
+	local current_buf = vim.api.nvim_get_current_buf()
+	vim.cmd("bnext")
+	vim.api.nvim_buf_delete(current_buf, { force = false })
+end, { desc = "Delete previous buffer (keep window)" })
 
 map("n", "<leader>qba", function()
 	for _, buf in ipairs(buffer.get_bufs()) do
