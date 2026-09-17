587dacf1ead65b0c38c6ce302a368d3f236bece0
PokumeKachi <108186213+PokumeKachi@users.noreply.github.com>
2026-09-17 21:18:30 +0700

    feat: yo we need project.nvim actually for lazygit and all that

--- config/nvim/lua/plugins/navigation/init.lua
+++ config/nvim/lua/plugins/navigation/init.lua
@@ -4,6 +4,6 @@ return {
     require("plugins.navigation.nvim_winpick"),
     require("plugins.navigation.oil"),
     require("plugins.navigation.outline"),
-    -- require("plugins.navigation.project"), -- does nothing other than cd-ing into the root dir
+    require("plugins.navigation.project"),
     require("plugins.navigation.winmove"),
 }
